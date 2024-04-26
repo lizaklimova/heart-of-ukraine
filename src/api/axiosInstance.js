1855dfbc24d23ef8a7bd247bc11e6adab688fb7a
Elizabeth Klymova <lozaklimovaaadd@gmail.com>
2024-04-26 21:59:38 +0300

    fix backend url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // export const baseURL = "http://localhost:8080/api/v1/";
 // export const baseURL = "https://www.heart-of-ukraine.com:8080/";
 // export const baseURL = "http://16.171.224.180:8080/api/v1/";
-export const baseURL = "http://www.heart-of-ukraine.com:8080/api/v1/";
+export const baseURL = "https://www.heart-of-ukraine.com:8080/api/v1/";
 
 const axiosInstance = axios.create({
   baseURL,
